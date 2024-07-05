717f240d89f58f5201726d6d877b9ff08c7d3714
Thirunarayanan Balathandayuthapani <thiru@mariadb.com>
2024-07-05 14:53:49 +0530

    MDEV-34474 InnoDB: Failing assertion: stat_n_leaf_pages > 0 in ha_innobase::estimate_rows_upper_bound
    - Column stat_value and sample_size in mysql.innodb_index_stats
    table is declared as BIGINT UNSIGNED without any check constraint.
    user manually updates the value of stat_value and sample_size
    to zero. InnoDB aborts the server while reading the statistics
    information because InnoDB expects at least one leaf
    page to exist for the index.
    
    - To fix this issue, InnoDB should interpret the value of
    stat_n_leaf_pages, stat_index_size in innodb_index_stats
    stat_clustered_index_size, stat_sum_of_other_index_sizes
    in innodb_table_stats as valid one even though user
    mentioned it as 0.

--- storage/innobase/dict/dict0stats.cc
+++ storage/innobase/dict/dict0stats.cc
@@ -2665,25 +2665,37 @@ dict_stats_fetch_table_stats_step(
 			break;
 
 		case 1: /* mysql.innodb_table_stats.clustered_index_size */
-
+		{
 			ut_a(dtype_get_mtype(type) == DATA_INT);
 			ut_a(len == 8);
+
+			ulint stat_clust_idx_size
+				= (ulint) mach_read_from_8(data);
 
 			table->stat_clustered_index_size
-				= (ulint) mach_read_from_8(data);
-
+				= (stat_clust_idx_size == 0)
+				  ? 1
+				  : stat_clust_idx_size;
 			break;
+		}
 
 		case 2: /* mysql.innodb_table_stats.sum_of_other_index_sizes */
-
+		{
 			ut_a(dtype_get_mtype(type) == DATA_INT);
 			ut_a(len == 8);
 
-			table->stat_sum_of_other_index_sizes
+			ulint stat_other_idx_size
 				= (ulint) mach_read_from_8(data);
+			if (!stat_other_idx_size
+			    && UT_LIST_GET_LEN(table->indexes) > 1) {
+				stat_other_idx_size
+					= UT_LIST_GET_LEN(table->indexes) - 1;
+			}
+			table->stat_sum_of_other_index_sizes
+				= stat_other_idx_size;
 
 			break;
-
+		}
 		default:
 
 			/* someone changed SELECT
@@ -2866,12 +2878,14 @@ dict_stats_fetch_index_stats_step(
 
 	if (stat_name_len == 4 /* strlen("size") */
 	    && strncasecmp("size", stat_name, stat_name_len) == 0) {
-		index->stat_index_size = (ulint) stat_value;
+		index->stat_index_size
+			= (ulint) stat_value == 0 ? 1 : stat_value;
 		arg->stats_were_modified = true;
 	} else if (stat_name_len == 12 /* strlen("n_leaf_pages") */
 		   && strncasecmp("n_leaf_pages", stat_name, stat_name_len)
 		   == 0) {
-		index->stat_n_leaf_pages = (ulint) stat_value;
+		index->stat_n_leaf_pages
+			= (ulint) stat_value == 0 ? 1 : stat_value;
 		arg->stats_were_modified = true;
 	} else if (stat_name_len == 12 /* strlen("n_page_split") */
 		   && strncasecmp("n_page_split", stat_name, stat_name_len)
@@ -2951,7 +2965,8 @@ dict_stats_fetch_index_stats_step(
 		index->stat_n_diff_key_vals[n_pfx - 1] = stat_value;
 
 		if (sample_size != UINT64_UNDEFINED) {
-			index->stat_n_sample_sizes[n_pfx - 1] = sample_size;
+			index->stat_n_sample_sizes[n_pfx - 1] =
+				sample_size == 0 ? 1 : sample_size;
 		} else {
 			/* hmm, strange... the user must have UPDATEd the
 			table manually and SET sample_size = NULL */
